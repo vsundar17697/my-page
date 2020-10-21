dea03628c0262f3d4f21e4ec031f1a3ccbb3a8ac
Sundararaman Venkataramani <sundararamanvenkataramani@MacBook-Pro.local>
2020-10-21 22:31:52 +0530

    Updates

---
+++ precache-manifest.152009e5a43f572bfad6b623235c33de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b2ce0811dbe42fc1325a56288b0dac",
+    "url": "/my-page/index.html"
+  },
+  {
+    "revision": "fa1722e553b3d43f52db",
+    "url": "/my-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ef578bcdb817bc3e5b3",
+    "url": "/my-page/static/js/2.f414f918.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/my-page/static/js/2.f414f918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1722e553b3d43f52db",
+    "url": "/my-page/static/js/main.4eb270c0.chunk.js"
+  },
+  {
+    "revision": "24a74e670725a91afbb1",
+    "url": "/my-page/static/js/runtime-main.9afb9fb7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
